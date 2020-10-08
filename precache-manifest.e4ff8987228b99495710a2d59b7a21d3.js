af01a52696434653bd87351f09f70a4b8a41491c
RodneyBedell <rodneybedell@gmail.com>
2020-10-08 15:15:20 -0400

    Updates

---
+++ precache-manifest.e4ff8987228b99495710a2d59b7a21d3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "66a7a0edd0fd1be8055179f7347f1bc4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b20ef2a4c8b394345efc",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "8cc7d58c029d13227aec",
+    "url": "/static/js/2.ec26fa20.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.ec26fa20.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b20ef2a4c8b394345efc",
+    "url": "/static/js/main.af617a7c.chunk.js"
+  },
+  {
+    "revision": "2c00d26fe84df26c97ad",
+    "url": "/static/js/runtime-main.e4748161.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
